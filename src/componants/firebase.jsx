6556a493a529d4001e806c15acb466bf63a8250b
Marcomagdy908 <146769197+Marcomagdy908@users.noreply.github.com>
2025-10-12 11:25:36 +0300

    Update firebase.jsx

--- src/componants/firebase.jsx
+++ src/componants/firebase.jsx
@@ -3,14 +3,14 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.REACT_APP_API_KEY,
-  authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.REACT_APP_PROJECT_ID,
-  storageBucket: import.meta.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.REACT_APP_APP_ID,
-  measurementId:import.meta.env.REACT_APP_MEASUREMENT_ID,
-  DataBaseURL: import.meta.env.REACT_APP_DATABASE_URL,
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  DataBaseURL: import.meta.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
